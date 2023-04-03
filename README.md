cb4170a38403f4a78fe48a9f089821bb7902dc17
Kelsey Abbey <124820451+kelseyabbey@users.noreply.github.com>
2023-04-03 12:21:47 -0600

    final updates

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # python-api-challenge
+This code analyzes different weather patterns for certain cities to determine the best place to go on vacation.
---
+++ api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "2cea5a6e2889ab51e13fd822175bcbb1"
+
+# Geoapify API Key
+geoapify_key = "cf50f91034f44ee6aa6cefcde7117014"
